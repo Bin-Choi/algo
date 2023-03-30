cb97686ebbef2ef15cb80ab2f2e076e62499fc2e
최성빈 <csb5268@gmail.com>
2023-03-31 00:00:24 +0900

    재귀함수

---
+++ SM 대비/1629.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+A, B, C = map(int, input().split())
+
+
+def DaC(a, b):
+    
+    if b == 1:
+        return a % C
+
+    temp = DaC(a, b//2)
+
+    if b % 2 == 0:
+        return temp * temp % C
+
+    else:
+        return temp * temp * a % C
+
+
+print(DaC(A, B))
