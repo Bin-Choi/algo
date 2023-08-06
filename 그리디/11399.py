4e8caf7d1d85aa5ada3b8d2a1fee6b5165b3a81f
최성빈 <csb5268@gmail.com>
2023-08-06 16:32:57 +0900

    0806 greedy

--- 그리디/11399.py
+++ 그리디/11399.py
@@ -1,3 +1,19 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+N = int(input())
+lst = list(map(int, input().split()))
+dp = [0]*N
+lst.sort()
+
+dp[0] = lst[0]
+for i in range(1, N):
+    dp[i] = dp[i-1] + lst[i]
+
+print(sum(dp))
+
+ans = 0
+for i in range(0, N):
+    ans += (N-i) * lst[i]
+
+print(ans)
